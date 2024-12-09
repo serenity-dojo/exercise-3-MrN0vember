7b9e52cb7c370f9decb4537ba24909d5e0fc6d3e
james <james.predwards@outlook.com>
2024-12-09 12:37:35 +0000

    tests passing

---
+++ src/main/java/com/serenitydojo/Dog.java
@@ -0,0 +1,30 @@
+package com.serenitydojo;
+
+public class Dog {
+    private String name;
+    private toy;
+    private int age;
+
+    // consturctor method
+    public Dog(String name, String toy, int age) {
+        this.name = name;
+        this.toy = toy;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavoriteToy() {
+        return toy;
+    }
+
+    public int getAge() {
+        return age;
+    }
+}
+
+
+
+
